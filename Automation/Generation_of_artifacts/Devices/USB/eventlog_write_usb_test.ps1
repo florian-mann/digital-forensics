f8c36e23b7c933f34c78867e446717567a9943e0
florian-mann <kontakt@florian-mann.de>
2021-09-05 11:24:33 +0200

    USB EventLogs

---
+++ Automation/Generation_of_artifacts/Devices/USB/eventlog_write_usb_test.ps1
@@ -0,0 +1,7 @@
+﻿#Configuration
+    $EventProvider = "Microsoft-Windows-DriverFrameworks-UserMode/Operational"
+    $EventID = 10000
+        $EventPayload_DeviceId = "SWD\WPDBUSENUM\_??_USBSTOR#DISK&VEN_KINGSTON&PROD_DATATRAVELER_2.0&REV_PMAP#00241D8CE459BFB0194935D4&0#{53F56307-B6BF-11D0-94F2-00A0C91EFB8B}"
+        $EventPayload_FrameworkVersion = "2.31.0"
+#Add Eventlog Entry
+New-WinEvent -ProviderName $EventProvider -Id $EventID -Payload @($EventPayload_DeviceId, $EventPayload_FrameworkVersion)
---
+++ Automation/Generation_of_artifacts/Devices/USB/invoke_eventlog_test.ps1
@@ -0,0 +1,13 @@
+﻿#Configuration
+    #Path to PsExec
+    $PsExecPath = "C:\tools\PsExec.exe"
+    $ScriptAsSystemPath = "C:\Users\semda\Documents\eventlog_write_usb_test.ps1"
+
+#Run Powershell-Script as SYSTEM
+    $process = Start-Process -PassThru -FilePath cmd.exe -Verb Runas -ArgumentList "/k $($PsExecPath) -i -s powershell.exe -ExecutionPolicy Bypass -Command ""$($ScriptAsSystemPath)"""
+    Start-Sleep -Seconds 5
+
+# Stop all Processes (-Name cmd) which did not started before RunAs
+
+    Stop-Process $process
+
