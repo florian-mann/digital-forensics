b832625cd766c55af35b257e0ca8413335505a92
florian-mann <kontakt@florian-mann.de>
2021-09-17 12:41:58 +0200

    Focus correctly

--- Automation/Generation_of_artifacts/Explorer/explorer.ps1
+++ Automation/Generation_of_artifacts/Explorer/explorer.ps1
@@ -1,3 +1,10 @@
+#Konsole nicht anzeigen wenn nicht in ISE ausgefuehrt
+    if(!($PSISE))
+    {
+            Add-Type -Name win -MemberDefinition '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);' -Namespace native
+            [native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle,0)
+    }
+    
 function Copy-fmItemUsingExplorer{
 # Opens Explorer with SourceFile selected, copies SourceFile to DestinationFolder, closes this Explorer Windows and reopens
 # Explorer with Destination File selected and closes Explorer Windows again.
@@ -7,6 +14,10 @@ function Copy-fmItemUsingExplorer{
         # "16" -> Yes to all
         # "80" -> Yes to all and Preserve undo information if possible
 # Existing files are overwritten without question!
+
+#Notes
+    # Open Windows in PowerShell https://devblogs.microsoft.com/scripting/hey-scripting-guy-how-can-i-use-windows-powershell-to-get-a-list-of-all-the-open-windows-on-a-computer/
+        #$a = New-Object -com "Shell.Application"; $b = $a.windows() | select-object LocationName; $b
 
 #If Show FullPath in Window Title is not set to true -> localized Folders (Documents/Dokumente -> EN/DE) make problems closing windows by title
 
@@ -35,9 +46,6 @@ function Copy-fmItemUsingExplorer{
             Start-Sleep -Seconds 5        
         }
 
-    #load VB -> Put outside of the Funktion????
-        Add-Type -AssemblyName Microsoft.VisualBasic
-        Add-Type -AssemblyName System.Windows.Forms
     #Create Destination Folder if not exists
     if(!(Test-Path $DestinationFolderPath) -and $CreateDestination)
     {
@@ -47,33 +55,30 @@ function Copy-fmItemUsingExplorer{
     {
         #Top Foldername to identifiy Windows Explorer WindowTitle
             #If Show FullPath in Window Title is not set to true
-                #$SourceExplorerWindowsTitle = Split-Path -Path (Split-Path -Path $SourceFilePath) -Leaf
-                #$DestinationExplorerWindowsTitle = Split-Path -Path $DestinationFolderPath -Leaf
+                #$SourceExplorerWindowTitle = Split-Path -Path (Split-Path -Path $SourceFilePath) -Leaf
+                #$DestinationExplorerWindowTitle = Split-Path -Path $DestinationFolderPath -Leaf
             #If Show FullPath in Window Title is set to true
-                $SourceExplorerWindowsTitle = Split-Path -Path $SourceFilePath
+                $SourceExplorerWindowTitle = Split-Path -Path $SourceFilePath
                 $pathlength = 0
                 $pathlength = $DestinationFolderPath.Length
                 if($pathlength -gt 0 -and $pathlength -le 3)
-                    { $DestinationExplorerWindowsTitle = $DestinationFolderPath}
+                    { $DestinationExplorerWindowTitle = $DestinationFolderPath}
                 else
-                    { $DestinationExplorerWindowsTitle = Split-Path -Path $DestinationFolderPath }
+                    { $DestinationExplorerWindowTitle = Split-Path -Path $DestinationFolderPath }
         #Open Explorer select file, to generate artifacts, for example Thumbnails
             #Parameters to launch explorer.exe with
             $Params = @()
                 $Params += "/select,"
                 $Params += "$($SourceFilePath)"
             Start-Process explorer.exe $Params #Start-Process explorer.exe -ArgumentList '/select, ""C:\Users\hanshack\Pictures\Product.bmp""'
-            Start-Sleep -Milliseconds 3000
-        #Focus Explorer Window
-            $wshell = New-Object -ComObject wscript.shell
-            $wshell.AppActivate($SourceExplorerWindowsTitle) | Out-Null
             Start-Sleep -Milliseconds 2000
         #Copy File with Explorer (COM) - not via Copy-Item, maybe this generates more User like artifacts...
             $objShell = New-Object -ComObject 'Shell.Application'    
             $objFolder = $objShell.NameSpace((Get-Item $DestinationFolderPath).FullName)
             $objFolder.CopyHere((Get-Item $SourceFilePath).FullName,$CopyFlags) #Blocks until finished
+            Start-Sleep -Milliseconds 1000
         #Close Explorer Window
-            Get-Process -Name explorer | Where-Object {$_.MainWindowTitle -eq $SourceExplorerWindowsTitle} | Stop-Process
+            Get-Process -Name explorer | Where-Object {$_.MainWindowTitle -eq $SourceExplorerWindowTitle} | Stop-Process
 
         #Open Explorer select file, to generate artifacts, for example Thumbnails
             #Check if Path is Volume Root
@@ -90,18 +95,33 @@ function Copy-fmItemUsingExplorer{
                 $Params += "/select,"
                 $Params += "$($DestinationFolderPath)$(Split-Path -Path $SourceFilePath -Leaf)"
             Write-Host $Params
+            Start-Sleep -Milliseconds 500
             Start-Process explorer.exe $Params #Start-Process explorer.exe -ArgumentList '/select, ""C:\Users\hanshack\Pictures\Product.bmp""'
-            Start-Sleep -Milliseconds 3000
+           Start-Sleep -Milliseconds 2000
+
         #Focus Explorer Window
             #Check if DestinationFolderPath is Root of Volume
                 #if($volumelabel -ne "")
-                #{ $DestinationExplorerWindowsTitle = "$($volumelabel) ($($DestinationFolderPath[0]):)" } # e.g.: "DATA (E:)"
+                #{ $DestinationExplorerWindowTitle = "$($volumelabel) ($($DestinationFolderPath[0]):)" } # e.g.: "DATA (E:)"
             
-            $wshell = New-Object -ComObject wscript.shell
-            $wshell.AppActivate($DestinationExplorerWindowsTitle) | Out-Null
+            #$wshell = New-Object -ComObject wscript.shell
+            #$wshell.AppActivate($DestinationExplorerWindowTitle) | Out-Null
+                #$wshell = New-Object -ComObject wscript.shell
+                #$wshell.AppActivate($DestinationExplorerWindowTitle) | Out-Null
+                #Start-Sleep -Milliseconds 100
+            [System.Windows.Forms.SendKeys]::SendWait(“%{TAB}”)
             Start-Sleep -Milliseconds 2000
+
+            #Start Photos with Enter to generate Recent items entry (C:\Users\USERNAME\AppData\Roaming\Microsoft\Windows\Recent)
+            if($SourceFilePath -eq "C:\Users\hanshack\Pictures\Motor.JPG")
+            {
+                [System.Windows.Forms.SendKeys]::SendWait(“{ENTER}”)
+                Start-Sleep -Milliseconds 10000
+                Get-Process -Name Microsoft.Photos | Stop-Process  # FileName is not in WindowsTitle!
+            }
+
         #Close Explorer Window
-            Get-Process -Name explorer | Where-Object {$_.MainWindowTitle -eq $DestinationExplorerWindowsTitle} | Stop-Process
+            Get-Process -Name explorer | Where-Object {$_.MainWindowTitle -eq $DestinationExplorerWindowTitle} | Stop-Process
 
         #validation and return values
         if(Test-Path -Path "$($DestinationFolderPath)\$(Split-Path -Path $SourceFilePath -Leaf)")
@@ -115,6 +135,8 @@ function Copy-fmItemUsingExplorer{
         return $false
     }
 }
+
+Start-Sleep -Seconds 5
 
 #Identifiy Plugged in USB-Stick (only one plugged in suppoerted)
     $USBDriveLetter = (Get-WmiObject win32_volume | Where-Object {$_.DriveLetter -eq (Get-WmiObject -Query "Select * From Win32_LogicalDisk"`
@@ -130,15 +152,15 @@ function Copy-fmItemUsingExplorer{
     Copy-fmItemUsingExplorer -source $SourceFilePath -destination $DestinationFolderPath -CopyFlags 80
 
 ################
-    $SourceFilePath = "C:\Users\flo\Documents\customer-list.xlsx"
+    $SourceFilePath = "C:\Users\hanshack\Documents\customer-list.xlsx"
     Copy-fmItemUsingExplorer -source $SourceFilePath -destination $DestinationFolderPath -CopyFlags 80
 
 ################
-    $SourceFilePath = "C:\Users\flo\Documents\Electric_motors.pdf"
+    $SourceFilePath = "C:\Users\hanshack\Documents\Electric_motors.pdf"
     Copy-fmItemUsingExplorer -source $SourceFilePath -destination $DestinationFolderPath -CopyFlags 80
 
 ################
-    $SourceFilePath = "C:\Users\flo\Pictures\Motor.JPG"
+    $SourceFilePath = "C:\Users\hanshack\Pictures\Motor.JPG"
     Copy-fmItemUsingExplorer -source $SourceFilePath -destination $DestinationFolderPath -CopyFlags 80
 ################
 
@@ -147,11 +169,12 @@ function Copy-fmItemUsingExplorer{
         Start-Sleep -Milliseconds 10000
         Get-Process -Name EXCEL | Stop-Process
 
-#Open Photo
-    explorer.exe "$($USBDriveLetter)\Motor.JPG"
-        Start-Sleep -Milliseconds 10000
-        Get-Process -Name Microsoft.Photos | Stop-Process
+#Open Photo 
+    #Does not generate Recent items entry (C:\Users\USERNAME\AppData\Roaming\Microsoft\Windows\Recent)
+    #explorer.exe "$($USBDriveLetter)\Motor.JPG"
+        #Start-Sleep -Milliseconds 10000
+        #Get-Process -Name Microsoft.Photos | Stop-Process
 
-#Eject USB Drive
+Eject USB Drive
     $Eject =  New-Object -ComObject Shell.Application
     $Eject.NameSpace(17).ParseName($USBDriveLetter).InvokeVerb("Eject")
